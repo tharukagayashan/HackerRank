76a2a4f853e9da53fb2079375e815f28b11fe17f
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2021-07-27 23:54:29 +0530

    Update and rename Java Loops II to Java_Loops_II.java

--- Java/Java_Loops_II.java
+++ Java/Java_Loops_II.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-class Solution{
+class Java_Loops_II{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
